7c139d93e932d08347feaca301f08feaa8974277
Anton Hesse <hesse151@umn.edu>
2020-08-18 08:53:29 -0500

    Updated run_analysis script

--- run_analysis.R
+++ run_analysis.R
@@ -13,4 +13,182 @@ if(!file.exists("UCI HAR Dataset")) {
     unzip("data_raw/activity.zip")
 }
 
-train <- read
+#### TRAINING SET ############################################################
+
+# load training set
+train <- read.table("UCI HAR Dataset/train/X_train.txt", header = FALSE)
+dim(train)
+str(train)
+
+# load feature names
+features <- read.table("UCI HAR Dataset/features.txt", header = FALSE,
+                       col.names = c("feature_number", "feature_name"))
+dim(features)
+str(features)
+
+# add feature names to column names of training set
+colnames(train) <- features$feature_name
+names(train)
+
+# load subjects
+subjectsTrain <- read.table("UCI HAR Dataset/train/subject_train.txt",
+                       header = FALSE, col.names = "subject_id")
+head(subjects)
+tail(subjects)
+dim(subjects)
+
+# cbind subject id's to training set
+if(!any(colnames(train) == "subject_id")) {
+    train <- cbind(subjects, train)
+}
+
+# load activities
+activitiesTraining <- read.table("UCI HAR Dataset/train/y_train.txt",
+                                 header = FALSE, col.names = c("activity"))
+dim(activitiesTraining)
+colnames(activitiesTraining)
+
+# load activity labels
+activityLabels <- read.table("UCI HAR Dataset/activity_labels.txt",
+                             header = FALSE,
+                             col.names = c("activity_number", "activity_name"))
+# make activity names lowercase for ease of typing
+activityLabels$activity_name <- tolower(activityLabels$activity_name)
+dim(activityLabels)
+
+# we found out later that there are some duplicate column names, so in order to recode the numbers as the activity label names we need to do so BEFORE cbinding the activities with the entire training set.
+
+duiplicateNames <- which(duplicated(colnames(train)))
+sort(colnames(train)[duiplicateNames])
+
+activitiesTraining <- activitiesTraining %>% 
+    mutate(activity = case_when(
+        activity == 1 ~ activityLabels$activity_name[1],
+        activity == 2 ~ activityLabels$activity_name[2],
+        activity == 3 ~ activityLabels$activity_name[3],
+        activity == 4 ~ activityLabels$activity_name[4],
+        activity == 5 ~ activityLabels$activity_name[5],
+        activity == 6 ~ activityLabels$activity_name[6]
+    )) %>% 
+    print
+
+# cbind subject id's to training set
+if(!any(colnames(train) == "activity")) {
+    train <- cbind(activitiesTraining, train)
+}
+
+if(!any(colnames(train) == "setType")) {
+    train$setType = "train"
+}
+
+
+#### TESTING SET ############################################################
+
+
+# load training set
+test <- read.table("UCI HAR Dataset/test/X_test.txt", header = FALSE)
+dim(test)
+str(test)
+
+# load feature names
+features <- read.table("UCI HAR Dataset/features.txt", header = FALSE,
+                       col.names = c("feature_number", "feature_name"))
+dim(features)
+str(features)
+
+# add feature names to column names of testing set
+colnames(test) <- features$feature_name
+names(test)
+
+# load subjects
+subjectsTest <- read.table("UCI HAR Dataset/test/subject_test.txt",
+                       header = FALSE, col.names = "subject_id")
+head(subjectsTest)
+tail(subjectsTest)
+dim(subjectsTest)
+
+# cbind subject id's to testing set
+if(!any(colnames(test) == "subject_id")) {
+    test <- cbind(subjectsTest, test)
+}
+
+# load activities
+activitiesTesting <- read.table("UCI HAR Dataset/test/y_test.txt",
+                                 header = FALSE, col.names = c("activity"))
+dim(activitiesTesting)
+colnames(activitiesTesting)
+
+# load activity labels
+activityLabels <- read.table("UCI HAR Dataset/activity_labels.txt",
+                             header = FALSE,
+                             col.names = c("activity_number", "activity_name"))
+# make activity names lowercase for ease of typing
+activityLabels$activity_name <- tolower(activityLabels$activity_name)
+dim(activityLabels)
+
+# we found out later that there are some duplicate column names, so in order to recode the numbers as the activity label names we need to do so BEFORE cbinding the activities with the entire testing set.
+
+duiplicateNames <- which(duplicated(colnames(test)))
+sort(colnames(test)[duiplicateNames])
+
+activitiesTesting <- activitiesTesting %>% 
+    mutate(activity = case_when(
+        activity == 1 ~ activityLabels$activity_name[1],
+        activity == 2 ~ activityLabels$activity_name[2],
+        activity == 3 ~ activityLabels$activity_name[3],
+        activity == 4 ~ activityLabels$activity_name[4],
+        activity == 5 ~ activityLabels$activity_name[5],
+        activity == 6 ~ activityLabels$activity_name[6]
+    ))
+
+# cbind subject id's to testing set
+if(!any(colnames(test) == "activity")) {
+    test <- cbind(activitiesTesting, test)
+}
+
+if(!any(colnames(test) == "setType")) {
+    test$setType = "test"
+}
+
+### combine training and testing sets
+dim(train)
+dim(test)
+combined <- rbind(train, test)
+dim(combined)
+
+##############################################################################
+# Find mean and sd values
+
+#### Mean
+meanMeasures <- combined[,grep("[mM]ean", colnames(combined))]
+meanMeasures <- cbind(combined$subject_id, combined$activity,
+                      combined$setType, meanMeasures)
+
+meanMeasures <- meanMeasures %>% 
+    rename("subject_id" = "combined$subject_id",
+           "activity" = "combined$activity",
+           "set_type" = "combined$setType")
+colnames(meanMeasures)[1:3]
+
+#### mean summary
+meanSmry <- meanMeasures %>% 
+    group_by(activity, subject_id) %>% 
+    summarize_each(funs(mean), -set_type)
+View(meanSmry)
+
+
+
+#### Std
+stdMeasures <- combined[,grep("[sS]td", colnames(combined))]
+stdMeasures <- cbind(combined$subject_id, combined$activity,
+                     combined$setType, stdMeasures)
+stdMeasures <- stdMeasures %>% 
+    rename("subject_id" = "combined$subject_id",
+           "activity" = "combined$activity",
+           "set_type" = "combined$setType")
+colnames(stdMeasures)[1:3]
+
+stdSmry <- stdMeasures %>% 
+    group_by(activity, subject_id) %>% 
+    summarize_each(funs(mean), -set_type)
+View(stdSmry)
